1e7180f4bcb90b3a77dc65b25b3e2085da4b3b2d
Ashley Denise Feliciano <111411686+aozicode@users.noreply.github.com>
2023-03-13 22:16:29 +0800

    Update activity_2.py

--- Pages/activity_2.py
+++ Pages/activity_2.py
@@ -1,45 +1,46 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib.pyplot as plt
 
-def change(x, y, ColorVal, MoveDirection, two_d_arr):
+plt.title("Flood Fill")
+
+two_d_arr = np.array([[1, 0, 1], 
+                      [0, 1, 0],
+                      [1, 0, 1]])
+
+def change(x, y, ColorVal, MoveDirection):
+    global two_d_arr
+
     if MoveDirection not in ["u", "d", "l", "r"]:
-        st.warning("Invalid MoveDirection. Try again.")
+        st.write("Invalid MoveDirection. Try again.")
         return
 
     if MoveDirection == "u" and x > 0:
         two_d_arr[x][y] = ColorVal
-        change(x - 1, y, ColorVal, MoveDirection, two_d_arr)
+        change(x - 1, y, ColorVal, MoveDirection)
     elif MoveDirection == "d" and x < two_d_arr.shape[0] - 1:
         two_d_arr[x][y] = ColorVal
-        change(x + 1, y, ColorVal, MoveDirection, two_d_arr)
+        change(x + 1, y, ColorVal, MoveDirection)
     elif MoveDirection == "r" and y < two_d_arr.shape[1] - 1:
         two_d_arr[x][y] = ColorVal
-        change(x, y + 1, ColorVal, MoveDirection, two_d_arr)
+        change(x, y + 1, ColorVal, MoveDirection)
     elif MoveDirection == "l" and y > 0:
         two_d_arr[x][y] = ColorVal
-        change(x, y - 1, ColorVal, MoveDirection, two_d_arr)
+        change(x, y - 1, ColorVal, MoveDirection)
 
-    img = plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    fig, ax = plt.subplots()
+    img = ax.imshow(two_d_arr, interpolation='none', cmap='plasma')
     img.set_clim([0, 50])
     plt.colorbar()
-    st.pyplot()
+    st.pyplot(fig)
 
-def app():
-    plt.title("Flood Fill")
-
-    two_d_arr = np.array([[1, 0, 1], 
-                          [0, 1, 0],
-                          [1, 0, 1]])
-
-    st.write("### Input")
-    column = st.slider("Column", 0, 2, 1)
-    row = st.slider("Row", 0, 2, 1)
-    color = st.slider("Color", 0, 50, 1)
-    direction = st.selectbox("Direction", ["u", "d", "l", "r"])
-
-    st.write("### Output")
-    change(column, row, color, direction, two_d_arr)
+def main():
+    column = st.number_input("Column (0, 1, 2): ", min_value=0, max_value=2, step=1)
+    row = st.number_input("Row (0, 1, 2): ", min_value=0, max_value=2, step=1)
+    color = st.number_input("Color: ")
+    direction = st.selectbox("Direction: ", ["u", "d", "l", "r"])
+    
+    change(column, row, color, direction)
 
 if __name__ == '__main__':
-    app()
+    main()
